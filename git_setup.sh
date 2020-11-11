8bf5748f28f298b88bc2b64813c31cc9168682d1
Thomas Jost <thomas.jost78@gmail.com>
2020-11-11 18:54:57 +0100

    Change git add scope to add online gitconfig file

--- git_setup.sh
+++ git_setup.sh
@@ -7,7 +7,7 @@ read email
 git config --global user.email $email
 git config --global user.name $full_name
 
-git add .
+git add gitconfig
 git commit --message "My user identity in the gitconfig"
 git push origin master
 
