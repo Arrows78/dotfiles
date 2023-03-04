e460f292af2d8ae24785e25a413050039d437e95
Thomas Jost <thomas.jost78@gmail.com>
2023-03-04 18:45:46 +0100

    Update brew list

--- brew.sh
+++ brew.sh
@@ -6,19 +6,48 @@ brew upgrade
 
 cask_args appdir: '/Applications'
 
+#############################################################
 # Apps
+#############################################################
+brew install 'git'
+brew install 'imagemagick'
+brew install 'mas'
+brew install 'tree'
+brew install 'wget'
+
+#############################################################
+# Desktop Apps
+#############################################################
+brew cask install 'alfred'
+brew cask install 'alt-tab'
 brew cask install 'bitwarden'
+brew cask install 'cron'
+brew cask install 'discord'
 brew cask install 'figma'
 brew cask install 'firefox'
+brew cask install 'github'
 brew cask install 'google-backup-and-sync'
 brew cask install 'google-chrome'
+brew cask install 'google-drive'
 brew cask install 'iterm2'
+brew cask install 'jumpcutony'
+brew cask install 'messenger'
+brew cask install 'notion'
+brew cask install 'onyx'
 brew cask install 'postman'
+brew cask install 'rectangle'
+brew cask install 'shottr'
 brew cask install 'slack'
 brew cask install 'spotify'
+brew cask install 'stats'
 brew cask install 'sublime-text'
+brew cask install 'vanilla'
 brew cask install 'vlc'
+brew cask install 'whatsapp'
+brew cask install 'zoom'
 
+#############################################################
 # Fonts
+#############################################################
 brew cask install 'font-open-sans'
 brew cask install 'font-roboto'
