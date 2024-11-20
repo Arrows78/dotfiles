e6321cb74b9ffc0396ca0dc2ac5b575fd47a2316
Thomas Jost <thomas.jost78@gmail.com>
2024-11-20 22:36:56 +0100

    Add custom pre-commit hook setup for tracking
    - Added a `.githooks` directory to store custom Git hooks
    - Created `hooks.sh` script to symlink hooks into `.git/hooks` directory
    - Ensured that the hooks directory exists before creating symlinks
    - Tracked the `.githooks/` directory and `hooks.sh` file for sharing with others

---
+++ hooks.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+CURRENT_DIR="$PWD"
+HOOKS_DIR=".githooks"
+TARGET_HOOKS_DIR=".git/hooks"
+
+# Ensure hooks directory exists
+mkdir -p "$HOOKS_DIR"
+
+# Link all custom hooks to the Git hooks directory
+for hook in "$HOOKS_DIR"/*; do
+    hook_name=$(basename "$hook")
+
+    LINK=$TARGET_HOOKS_DIR/$hook_name
+
+    if [ ! -e "$LINK" ]; then
+        ln -s "$CURRENT_DIR/$hook" "$LINK"
+        echo "-----> Symlinking $LINK in Git hooks"
+    fi
+done
